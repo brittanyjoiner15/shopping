0b5883911f466b4946d1501779a12559f0637d2e
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-04-14 21:36:16 -0500

    additional styling and edits

--- shopping-app/src/ItemBox.js
+++ shopping-app/src/ItemBox.js
@@ -4,21 +4,15 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import jsonData from "./shoppingList.json";
 
-//do i need to build a function that says for each object in the json file, make a ItemBox with this.item and this.price?
-
-//practice mapping
-
-const productList = jsonData.products;
-
 class Item extends React.Component {
   render() {
     return (
-      <div>
+      <div className="itemBox">
         {jsonData.Products.map((product, i) => {
           return (
             <div className="item">
-              <p>Product: {product.itemName}</p>
-              <p>Price: {product.price}</p>
+              <h2>{product.itemName}</h2>
+              <p>Price: ${product.price}</p>
               <button className="btn-primary mx-2">Buy</button>
               <button className="btn-warning">Sell</button>
               <br />
@@ -35,16 +29,5 @@ class Item extends React.Component {
     );
   }
 }
-
-//<p> Short Description: </p>
-//<h4>Price: ${jsonData[0].price}</h4>
-//<button className="btn-primary mx-3 my-3">Buy</button>
-//<button className="btn-warning">Sell</button>
-//<br />
-//<img
-//src="https://media.giphy.com/media/fAhOtxIzrTxyE/giphy.gif"
-//width="70%"
-//alt="pic"
-///>
 
 export default Item;
