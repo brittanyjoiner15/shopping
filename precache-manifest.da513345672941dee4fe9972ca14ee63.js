8278e3e9bda573ddbc3c7889697ace81c1de1da0
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-04-30 18:14:58 -0500

    Updates

---
+++ precache-manifest.da513345672941dee4fe9972ca14ee63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703ef21e6fac363a72de418465e8c97f",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "edcebe3b210a7aa0685c",
+    "url": "/react-tutorial/static/css/main.a033000e.chunk.css"
+  },
+  {
+    "revision": "77d6204d38d97f722a12",
+    "url": "/react-tutorial/static/js/2.b6563199.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.b6563199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edcebe3b210a7aa0685c",
+    "url": "/react-tutorial/static/js/main.e8b7aef8.chunk.js"
+  },
+  {
+    "revision": "42c5ba778d451c2e5ee6",
+    "url": "/react-tutorial/static/js/runtime-main.f27cc5f9.js"
+  }
+]);
