aeaf5ee9f10a56b5b94fd1bb3eca1f56e804ca38
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-04-30 16:27:57 -0500

    fixed the add button

--- shopping-app/src/App.js
+++ shopping-app/src/App.js
@@ -5,24 +5,24 @@ import { ItemBox } from "./ItemBox";
 import { Balance } from "./Balance";
 
 export class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       total: 0,
     };
+
+    this.onClick = this.onClick.bind(this);
+
+    //do we need to add this
   }
 
-  state = { total: 0 };
-
-  onClick = (e) => {
-    console.log(typeof e.currentTarget.value);
-    this.setState((prevState) => {
-      console.log(prevState.total);
-      return {
-        total: prevState.total + parseInt(e.currentTarget.value, 10),
-      };
-    });
+  onClick = (e, prevState) => {
+    console.log(typeof parseInt(e.target.value, 10));
+    console.log(typeof prevState);
+    //prev state is undefined, so there's something wrong with pulling that.
+    this.setState({ total: this.state.total + parseInt(e.target.value, 10) });
+    console.log(this.state.total);
   };
 
   render() {
--- shopping-app/src/Balance.js
+++ shopping-app/src/Balance.js
@@ -3,45 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-const total = 0;
-
 export class Balance extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      result: 0,
+      total: 0,
     };
-
-    this.onClick();
   }
 
-  onClick = (button) => {
-    if (button === "buy") {
-      this.result = this.result + 1;
-    } else if (button === "sell") {
-      this.reset();
-    } else {
-      this.setState({
-        result: this.state.result + button,
-      });
-    }
-  };
-
-  increase = () => {};
-
-  calculate = () => {
-    try {
-      this.setState({
-        // eslint-disable-next-line
-        result: (eval(this.state.result) || "") + "",
-      });
-    } catch (e) {
-      this.setState({
-        result: "error",
-      });
-    }
-  };
   render() {
     return (
       <div className="totalBox">
